7f40924934843374ad38e521c178fccc2ae86d91
Jun Nishimura <n.junjun0303@gmail.com>
2024-09-20 01:37:15 +0900

    add lexical analysis for integers

---
+++ lexer/lexer.go
@@ -0,0 +1,94 @@
+package lexer
+
+import "github.com/JunNishimura/jsop/token"
+
+type Lexer struct {
+	input   string
+	curPos  int
+	nextPos int
+	curChar byte
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.nextPos >= len(l.input) {
+		l.curChar = 0
+	} else {
+		l.curChar = l.input[l.nextPos]
+	}
+	l.curPos = l.nextPos
+	l.nextPos++
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.curChar {
+	case '{':
+		tok = newToken(token.LBRACE, l.curChar)
+	case '}':
+		tok = newToken(token.RBRACE, l.curChar)
+	case '"':
+		tok = newToken(token.DOUBLE_QUOTE, l.curChar)
+	case ':':
+		tok = newToken(token.COLON, l.curChar)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isDigit(l.curChar) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else if isLetter(l.curChar) {
+			tok.Literal = l.readString()
+			tok.Type = token.LookupStringTokenType(tok.Literal)
+			return tok
+		}
+		tok = newToken(token.ILLEGAL, l.curChar)
+	}
+
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.curChar == ' ' || l.curChar == '\t' || l.curChar == '\n' || l.curChar == '\r' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z'
+}
+
+func (l *Lexer) readNumber() string {
+	startPos := l.curPos
+	for isDigit(l.curChar) {
+		l.readChar()
+	}
+	return l.input[startPos:l.curPos]
+}
+
+func (l *Lexer) readString() string {
+	startPos := l.curPos
+	for isLetter(l.curChar) {
+		l.readChar()
+	}
+	return l.input[startPos:l.curPos]
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,48 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/JunNishimura/jsop/token"
+)
+
+func TestSingleProgram(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected []token.Token
+	}{
+		{
+			name: "integer atom",
+			input: `
+				{
+					"atom": 1
+				}`,
+			expected: []token.Token{
+				{Type: token.LBRACE, Literal: "{"},
+				{Type: token.DOUBLE_QUOTE, Literal: "\""},
+				{Type: token.ATOM, Literal: "atom"},
+				{Type: token.DOUBLE_QUOTE, Literal: "\""},
+				{Type: token.COLON, Literal: ":"},
+				{Type: token.INT, Literal: "1"},
+				{Type: token.RBRACE, Literal: "}"},
+				{Type: token.EOF, Literal: ""},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := New(tt.input)
+			for i, expected := range tt.expected {
+				tok := l.NextToken()
+				if tok.Type != expected.Type {
+					t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, expected.Type, tok.Type)
+				}
+				if tok.Literal != expected.Literal {
+					t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, expected.Literal, tok.Literal)
+				}
+			}
+		})
+	}
+}
