38a002f5f7be92cd47b527b52d84a773122606e5
Jun Nishimura <n.junjun0303@gmail.com>
2024-09-23 18:31:14 +0900

    define object package (#8)

---
+++ object/object.go
@@ -0,0 +1,39 @@
+package object
+
+import "fmt"
+
+const (
+	ERROR_OBJ   = "ERROR"
+	INTEGER_OBJ = "INTEGER"
+	BUILTIN_OBJ = "BUILTIN"
+)
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+type BuiltinFunction func(args ...Object) Object
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
