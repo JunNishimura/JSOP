d8a2351ee3103d7d454cf18696b4483c2c998e84
Jun Nishimura <n.junjun0303@gmail.com>
2024-10-12 00:27:29 +0900

    add macro system (#50)

---
+++ evaluator/macro_expansion.go
@@ -0,0 +1,182 @@
+package evaluator
+
+import (
+	"github.com/JunNishimura/jsop/ast"
+	"github.com/JunNishimura/jsop/object"
+)
+
+func DefineMacros(program ast.Expression, env *object.Environment) ast.Expression {
+	if arrayExp, ok := program.(*ast.Array); ok {
+		definitions := make([]int, 0)
+
+		for i, exp := range arrayExp.Elements {
+			if isMacroDefinition(exp) {
+				addMacro(exp, env)
+				definitions = append(definitions, i)
+			}
+		}
+
+		for i := len(definitions) - 1; i >= 0; i = i - 1 {
+			definitionIndex := definitions[i]
+			arrayExp.Elements = append(
+				arrayExp.Elements[:definitionIndex],
+				arrayExp.Elements[definitionIndex+1:]...,
+			)
+		}
+
+		return arrayExp
+	}
+
+	if isMacroDefinition(program) {
+		addMacro(program, env)
+	}
+
+	return nil
+}
+
+func isMacroDefinition(exp ast.Expression) bool {
+	kvObject, ok := exp.(*ast.KeyValueObject)
+	if !ok {
+		return false
+	}
+
+	_, ok = kvObject.KVPairs()["defmacro"]
+	return ok
+}
+
+func addMacro(exp ast.Expression, env *object.Environment) {
+	kvObject, ok := exp.(*ast.KeyValueObject)
+	if !ok {
+		return
+	}
+
+	defmacro, ok := kvObject.KVPairs()["defmacro"]
+	if !ok {
+		return
+	}
+	defmacroValue, ok := defmacro.(*ast.KeyValueObject)
+	if !ok {
+		return
+	}
+	kvPairs := defmacroValue.KVPairs()
+
+	nameValue, ok := kvPairs["name"]
+	if !ok {
+		return
+	}
+	macroName, ok := nameValue.(*ast.StringLiteral)
+	if !ok {
+		return
+	}
+
+	keys := make([]*ast.StringLiteral, 0)
+	keysValue, ok := kvPairs["keys"]
+	if ok {
+		if keysArray, ok := keysValue.(*ast.Array); ok {
+			for _, key := range keysArray.Elements {
+				strLiteral, ok := key.(*ast.StringLiteral)
+				if !ok {
+					return
+				}
+				keys = append(keys, strLiteral)
+			}
+		} else {
+			strLiteral, ok := keysValue.(*ast.StringLiteral)
+			if !ok {
+				return
+			}
+			keys = append(keys, strLiteral)
+		}
+	}
+
+	bodyValue, ok := kvPairs["body"]
+	if !ok {
+		return
+	}
+
+	macroObj := &object.Macro{
+		Keys: keys,
+		Body: bodyValue,
+		Env:  env,
+	}
+
+	env.Set(macroName.Value, macroObj)
+}
+
+func ExpandMacros(program ast.Expression, env *object.Environment) ast.Expression {
+	return ast.Modify(program, func(exp ast.Expression) ast.Expression {
+		kvObject, ok := exp.(*ast.KeyValueObject)
+		if !ok {
+			return exp
+		}
+
+		macroName, ok := isMacroCall(kvObject, env)
+		if !ok {
+			return exp
+		}
+		macroObj, ok := env.Get(macroName)
+		if !ok {
+			return exp
+		}
+		macro, ok := macroObj.(*object.Macro)
+		if !ok {
+			return exp
+		}
+
+		macroContentValue, ok := kvObject.KVPairs()[macroName]
+		if !ok {
+			return exp
+		}
+		macroContent, ok := macroContentValue.(*ast.KeyValueObject)
+		if !ok {
+			return exp
+		}
+		macroContentKV := macroContent.KVPairs()
+
+		newKV := map[string]*object.Quote{}
+		for _, key := range macro.Keys {
+			keyValue, ok := macroContentKV[key.Value]
+			if !ok {
+				return exp
+			}
+			newKV[key.Value] = &object.Quote{Expression: keyValue}
+		}
+
+		macroEnv := extendMacroEnv(macro, newKV)
+
+		evaluated := Eval(macro.Body, macroEnv)
+
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			return exp
+		}
+
+		return quote.Expression
+	})
+}
+
+func extendMacroEnv(macro *object.Macro, kv map[string]*object.Quote) *object.Environment {
+	extended := object.NewEnclosedEnvironment(macro.Env)
+	for _, key := range macro.Keys {
+		extended.Set(key.Value, kv[key.Value])
+	}
+	return extended
+}
+
+func isMacroCall(kvObj *ast.KeyValueObject, env *object.Environment) (string, bool) {
+	for key, _ := range kvObj.KVPairs() {
+		obj, ok := env.Get(key)
+		if !ok {
+			continue
+		}
+
+		_, ok = obj.(*object.Macro)
+		if !ok {
+			continue
+		}
+
+		return key, true
+	}
+
+	return "", false
+}
---
+++ evaluator/macro_expansion_test.go
@@ -0,0 +1,198 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/JunNishimura/jsop/ast"
+	"github.com/JunNishimura/jsop/lexer"
+	"github.com/JunNishimura/jsop/object"
+	"github.com/JunNishimura/jsop/parser"
+)
+
+func testParseProgram(input string) (ast.Expression, error) {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func TestDefineMacro(t *testing.T) {
+	input := `
+        {
+            "defmacro": {
+                "name": "myMacro",
+                "keys": ["foo", "bar"],
+                "body": {
+                    "command": {
+                        "symbol": "quote",
+                        "args": {
+                            "command": {
+                                "symbol": "+",
+                                "args": [",foo", ",bar"]
+                            }
+                        }
+                    }
+                }
+            }
+        }`
+
+	env := object.NewEnvironment()
+	exp, err := testParseProgram(input)
+	if err != nil {
+		t.Fatalf("parse error: %s", err)
+	}
+
+	_ = DefineMacros(exp, env)
+
+	obj, ok := env.Get("myMacro")
+	if !ok {
+		t.Fatalf("macro not in environment")
+	}
+
+	macro, ok := obj.(*object.Macro)
+	if !ok {
+		t.Fatalf("object is not Macro. got=%T (%+v)", obj, obj)
+	}
+
+	if len(macro.Keys) != 2 {
+		t.Fatalf("wrong number of macro Keys. want=2, got=%d", len(macro.Keys))
+	}
+
+	if macro.Keys[0].Value != "foo" {
+		t.Fatalf("key is not foo. got=%q", macro.Keys[0])
+	}
+	if macro.Keys[1].Value != "bar" {
+		t.Fatalf("key is not bar. got=%q", macro.Keys[1])
+	}
+
+	expectedBody := "{\"command\": {\"symbol\": \"quote\", \"args\": {\"command\": {\"symbol\": \"+\", \"args\": [\",foo\", \",bar\"]}}}}"
+	if macro.Body.String() != expectedBody {
+		t.Fatalf("body is not %q. got=%q", expectedBody, macro.Body.String())
+	}
+}
+
+func TestExpandMacro(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input: `
+                [
+                    {
+                        "defmacro": {
+                            "name": "hoge",
+                            "body": {
+                                "command": {
+                                    "symbol": "quote",
+                                    "args": {
+                                        "command": {
+                                            "symbol": "+",
+                                            "args": [1, 2]
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "hoge": {}
+                    }
+                ]`,
+			expected: "[{\"command\": {\"symbol\": \"+\", \"args\": [1, 2]}}]",
+		},
+		{
+			input: `
+                [
+                    {
+                        "defmacro": {
+                            "name": "reverse",
+                            "keys": ["first", "second"],
+                            "body": {
+                                "command": {
+                                    "symbol": "quote",
+                                    "args": {
+                                        "command": {
+                                            "symbol": "-",
+                                            "args": [",second", ",first"]
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    {
+                        "reverse": {
+                            "first": 2,
+                            "second": 3
+                        }
+                    }
+                ]`,
+			expected: "[{\"command\": {\"symbol\": \"-\", \"args\": [3, 2]}}]",
+		},
+		{
+			input: `
+                [
+                    {
+                        "defmacro": {
+                            "name": "unless",
+                            "keys": ["cond", "conseq", "alt"],
+                            "body": {
+                                "command": {
+                                    "symbol": "quote",
+                                    "args": {
+                                        "if": {
+                                            "cond": {"command": {"symbol": "!", "args": ",cond"}},
+                                            "conseq": ",conseq",
+                                            "alt": ",alt"
+                                        }
+                                    }
+                                } 
+                            }
+                        }
+                    },
+                    {
+                        "unless": {
+                            "cond": {
+                                "command": {
+                                    "symbol": ">",
+                                    "args": [2, 1]
+                                }
+                            },
+                            "conseq": {
+                                "command": {
+                                    "symbol": "print",
+                                    "args": "not greater"
+                                }
+                            },
+                            "alt": {
+                                "command": {
+                                    "symbol": "print",
+                                    "args": "greater"
+                                }
+                            }
+                        }
+                    }
+                ]`,
+			expected: "[{\"if\": {\"cond\": {\"command\": {\"symbol\": \"!\", \"args\": {\"command\": {\"symbol\": \">\", \"args\": [2, 1]}}}}, \"conseq\": {\"command\": {\"symbol\": \"print\", \"args\": \"not greater\"}}, \"alt\": {\"command\": {\"symbol\": \"print\", \"args\": \"greater\"}}}}]",
+		},
+	}
+
+	for _, tt := range tests {
+		expected, err := testParseProgram(tt.expected)
+		if err != nil {
+			t.Fatalf("parse error for expected: %s", err)
+		}
+		program, err := testParseProgram(tt.input)
+		if err != nil {
+			t.Fatalf("parse error for input: %s", err)
+		}
+
+		env := object.NewEnvironment()
+		DefineMacros(program, env)
+		expanded := ExpandMacros(program, env)
+
+		if expanded.String() != expected.String() {
+			t.Errorf("not equal. got=%q, want=%q", expanded.String(), expected.String())
+		}
+	}
+}
